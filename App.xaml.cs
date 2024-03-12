1cd8caca632ace106453ef65a18cfd4872259ec6
Thejoshuajose <joshuajose3@icloud.com>
2024-03-12 12:10:38 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Joshua_Gonzales___IST_311___LabControlsReseach
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+
+namespace Joshua_Gonzales___IST_311___LabControlsReseach
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Set the default selected index
+        }
+
+
+        private void btnToBe_Click(object sender, RoutedEventArgs e)
+        {
+            imgAsusRog.SetCurrentValue(OpacityProperty, 1.0);
+
+        }
+
+        private void btnNotToBe_Click(object sender, RoutedEventArgs e)
+        {
+            imgAsusRog.SetCurrentValue(OpacityProperty, 0.0);
+
+        }
+
+        private void btnSmwtToBe_Click(object sender, RoutedEventArgs e)
+        {
+            imgAsusRog.SetCurrentValue(OpacityProperty,0.5);
+        }
+    }
+}
